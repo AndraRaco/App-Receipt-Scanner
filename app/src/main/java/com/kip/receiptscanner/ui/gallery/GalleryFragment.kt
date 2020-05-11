22e68028d58190aa76149d74ab38cfef90be77c8
4Kip <cip.perju@gmail.com>
2020-05-11 18:02:16 +0300

    updated fragments

--- app/src/main/java/com/kip/receiptscanner/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/kip/receiptscanner/ui/gallery/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.kip.receiptscanner.ui.gallery
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kip.receiptscanner.R
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 class GalleryFragment : Fragment() {
 
@@ -29,5 +31,12 @@ class GalleryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
     }
+
+
 }
