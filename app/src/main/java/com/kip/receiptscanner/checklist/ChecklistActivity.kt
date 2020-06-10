72ae419209dd83ecc7dee37ed25327bf65eda114
Racovita Andra <andra.rac@gmail.com>
2020-06-10 16:15:58 +0300

    Rearrange functions in ChecklistActivity

--- app/src/main/java/com/kip/receiptscanner/checklist/ChecklistActivity.kt
+++ app/src/main/java/com/kip/receiptscanner/checklist/ChecklistActivity.kt
@@ -2,10 +2,8 @@ package com.kip.receiptscanner.checklist
 
 import android.os.Bundle
 import android.util.SparseBooleanArray
-import android.widget.ArrayAdapter
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.get
 import com.kip.receiptscanner.Product
 import com.kip.receiptscanner.R
 
@@ -14,7 +12,6 @@ import kotlinx.android.synthetic.main.activity_checklist.add
 import kotlinx.android.synthetic.main.activity_checklist.clear
 import kotlinx.android.synthetic.main.activity_checklist.delete
 import kotlinx.android.synthetic.main.app_bar_main_drawer.*
-import kotlinx.android.synthetic.main.list_products.view.*
 
 //import kotlinx.android.synthetic.main.fragment_slideshow.*
 
@@ -22,38 +19,33 @@ class ChecklistActivity : AppCompatActivity() {
 
 //    class Product(var name: String, var price: Double){}
 
+    private var itemlist = arrayListOf<Product>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checklist)
         setSupportActionBar(toolbar)
 
         // Initializing the array lists and the adapter
-
-        var itemlist = arrayListOf<Product>()
         listView.adapter = ProductAdapter(this, itemlist)
 
         // Adding the items to the list when the add button is pressed
         add.setOnClickListener {
-            val name = editText.text.toString()
-            val price = et_price.text.toString().toDouble()
-            val p =  Product(name, price)
-            itemlist.add(p)
-            //listView.adapter =  adapter
-            (listView.adapter as ProductAdapter).notifyDataSetChanged()
-            // This is because every time when you add the item the input space or the edit text space will be cleared
-            editText.text.clear()
-            et_price.text.clear()
+            addRow()
         }
 
         // Clearing all the items in the list when the clear button is pressed
         clear.setOnClickListener {
-            itemlist.clear()
-            (listView.adapter as ProductAdapter).notifyDataSetChanged()
+            clearRows()
         }
 
         // Adding the toast message to the list when an item on the list is pressed
         listView.setOnItemClickListener { adapterView, view, i, l ->
-            android.widget.Toast.makeText(this, "You Selected " + itemlist.get(i).name + " " + itemlist.get(i).price, android.widget.Toast.LENGTH_SHORT).show()
+            android.widget.Toast.makeText(
+                this,
+                "You Selected " + itemlist.get(i).name + " " + itemlist.get(i).price,
+                android.widget.Toast.LENGTH_SHORT
+            ).show()
             val position: SparseBooleanArray = listView.checkedItemPositions
 
             // Check the checkbox
@@ -64,38 +56,65 @@ class ChecklistActivity : AppCompatActivity() {
 
         // Selecting and Deleting the items from the list when the delete button is pressed
         delete.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    itemlist.removeAt(item)
-                }
-                item--
-            }
-            position.clear()
-            (listView.adapter as ProductAdapter).notifyDataSetChanged()
+            deleteRow()
         }
 
         // Calculate how much has the person to pay
         calculate.setOnClickListener {
-            val position: SparseBooleanArray = listView.checkedItemPositions
-            val count = listView.count
-            var sum: Double = 0.0
-
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    val priceOfProduct = itemlist[item].price
-                    sum += priceOfProduct
-                }
-                item--
-            }
-
-            android.widget.Toast.makeText(this, "You have to pay $sum", android.widget.Toast.LENGTH_SHORT).show()
+            calculatePriceToPay()
         }
 
+    }
+
+    fun addRow() {
+        val name = editText.text.toString()
+        val price = et_price.text.toString().toDouble()
+        val p = Product(name, price)
+        itemlist.add(p)
+        //listView.adapter =  adapter
+        (listView.adapter as ProductAdapter).notifyDataSetChanged()
+        // This is because every time when you add the item the input space or the edit text space will be cleared
+        editText.text.clear()
+        et_price.text.clear()
+    }
+
+    fun deleteRow() {
+        val position: SparseBooleanArray = listView.checkedItemPositions
+        val count = listView.count
+        var item = count - 1
+        while (item >= 0) {
+            if (position.get(item)) {
+                itemlist.removeAt(item)
+            }
+            item--
+        }
+        position.clear()
+        (listView.adapter as ProductAdapter).notifyDataSetChanged()
+    }
+
+    fun clearRows() {
+        itemlist.clear()
+        (listView.adapter as ProductAdapter).notifyDataSetChanged()
+    }
+
+    fun calculatePriceToPay() {
+        val position: SparseBooleanArray = listView.checkedItemPositions
+        val count = listView.count
+        var sum: Double = 0.0
+
+        var item = count - 1
+        while (item >= 0) {
+            if (position.get(item)) {
+                val priceOfProduct = itemlist[item].price
+                sum += priceOfProduct
+            }
+            item--
+        }
+
+        android.widget.Toast.makeText(
+            this,
+            "You have to pay $sum",
+            android.widget.Toast.LENGTH_SHORT
+        ).show()
     }
 }
