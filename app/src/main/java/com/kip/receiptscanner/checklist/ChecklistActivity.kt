75b44abdad5945eb3617bda50c07f7991ce8f17a
Racovita Andra <andra.rac@gmail.com>
2020-06-10 11:01:23 +0300

    Update CheckList
    - create ProductAdapter, list_products.xml
    - edit activity_checklist layout
    - use ProductAdapter in ChecklistActivity

--- app/src/main/java/com/kip/receiptscanner/checklist/ChecklistActivity.kt
+++ app/src/main/java/com/kip/receiptscanner/checklist/ChecklistActivity.kt
@@ -7,18 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kip.receiptscanner.R
 
 import kotlinx.android.synthetic.main.activity_checklist.*
+import kotlinx.android.synthetic.main.activity_checklist.add
+import kotlinx.android.synthetic.main.activity_checklist.clear
+import kotlinx.android.synthetic.main.activity_checklist.delete
 import kotlinx.android.synthetic.main.app_bar_main_drawer.*
+//import kotlinx.android.synthetic.main.fragment_slideshow.*
 
 class ChecklistActivity : AppCompatActivity() {
 
-    class Product(name: String, price: Double) {
-        var name : String = ""
-        var price : Double = 0.0
-
-        class Product(val name: String = "", val price: Double = 0.0){}
-
-    }
-
+    class Product(var name: String, var price: Double){}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,33 +23,39 @@ class ChecklistActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         // Initializing the array lists and the adapter
+
         var itemlist = arrayListOf<Product>()
-        var adapter = ArrayAdapter<Product>(this,
+        listView.adapter = ProductAdapter(this, itemlist)
+        /*var adapter = ArrayAdapter<Product>(this,
             android.R.layout.simple_list_item_multiple_choice
-            , itemlist)
+            , itemlist)*/
 
         // Adding the items to the list when the add button is pressed
         add.setOnClickListener {
 
-            var name : String  = editText.text.toString().substringBefore(' ')
-            var price : Double = editText.text.toString().substringAfter(' ').toDouble()
+            /*var name : String  = editText.text.toString().substringBefore(' ')
+            var price : Double = editText.text.toString().substringAfter(' ').toDouble()*/
+            val name = editText.text.toString()
+            val price = et_price.text.toString().toDouble()
             val p =  Product(name, price)
             itemlist.add(p)
-            listView.adapter =  adapter
-            adapter.notifyDataSetChanged()
+            //listView.adapter =  adapter
+            (listView.adapter as ProductAdapter).notifyDataSetChanged()
             // This is because every time when you add the item the input space or the edit text space will be cleared
             editText.text.clear()
+            et_price.text.clear()
         }
 
         // Clearing all the items in the list when the clear button is pressed
         clear.setOnClickListener {
             itemlist.clear()
-            adapter.notifyDataSetChanged()
+            (listView.adapter as ProductAdapter).notifyDataSetChanged()
         }
 
         // Adding the toast message to the list when an item on the list is pressed
         listView.setOnItemClickListener { adapterView, view, i, l ->
-            android.widget.Toast.makeText(this, "You Selected "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
+            android.widget.Toast.makeText(this, "You Selected " + itemlist.get(i).name + " " + itemlist.get(i).price, android.widget.Toast.LENGTH_SHORT).show()
+            val position: SparseBooleanArray = listView.checkedItemPositions
         }
 
         // Selecting and Deleting the items from the list when the delete button is pressed
@@ -63,12 +66,12 @@ class ChecklistActivity : AppCompatActivity() {
             while (item >= 0) {
                 if (position.get(item))
                 {
-                    adapter.remove(itemlist.get(item))
+                    itemlist.removeAt(item)
                 }
                 item--
             }
             position.clear()
-            adapter.notifyDataSetChanged()
+            (listView.adapter as ProductAdapter).notifyDataSetChanged()
         }
 
     }
