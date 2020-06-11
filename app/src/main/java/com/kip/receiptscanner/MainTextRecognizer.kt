adffda86ccd8c638adcd2194929bcaab6ddc1cb4
SusanRadu <45876287+SusanRadu@users.noreply.github.com>
2020-06-11 03:09:34 +0300

    Added interaction between scanner and checklist
    Now all the scanned items are automatically added to the checklist

--- app/src/main/java/com/kip/receiptscanner/MainTextRecognizer.kt
+++ app/src/main/java/com/kip/receiptscanner/MainTextRecognizer.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
@@ -13,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionText
+import com.kip.receiptscanner.checklist.ChecklistActivity
 import java.util.*
 
 
@@ -20,12 +20,17 @@ class MainTextRecognizer : AppCompatActivity() {
 
     lateinit var imageView: ImageView
     lateinit var editText: EditText
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.imageView)
         editText = findViewById(R.id.editText)
+
     }
 
 
@@ -55,7 +60,21 @@ class MainTextRecognizer : AppCompatActivity() {
             detector.processImage(image)
                 .addOnSuccessListener { firebaseVisionText ->
                     v.isEnabled = true
-                    processResultText(firebaseVisionText)
+
+                    var tes: List<String?>
+                    var test: List<String?>
+                    test = ArrayList(200)
+
+                    var produse : List<Pair<Double, String>>
+                    produse = processResultText(firebaseVisionText)
+
+
+                    for(x in produse)
+                        test.add(x.first.toString() + ',' +x.second)
+
+                    val i = Intent(this, ChecklistActivity::class.java)
+                    i.putExtra("key", test)
+                    startActivity(i)
                 }
                 .addOnFailureListener {
                     v.isEnabled = true
@@ -67,14 +86,19 @@ class MainTextRecognizer : AppCompatActivity() {
     }
 
 
-    private fun processResultText(resultText: FirebaseVisionText) {
+    private fun processResultText(resultText: FirebaseVisionText) : List<Pair<Double, String>> {
+
+
+        /*
         if (resultText.textBlocks.size == 0) {
             editText.setText("No Text Found")
             return
-        }
+        }*/
+
         for (block in resultText.textBlocks) {
             val blockText = block.text
             editText.append(blockText + "\n")
+            //tes.add(blockText)
         }
 
         val tuples = getTuples(editText.text.toString())
@@ -82,23 +106,25 @@ class MainTextRecognizer : AppCompatActivity() {
         for (t in tuples) {
             editText.append(t.toString() + "\n")
         }
+        return tuples
+
     }
 
-    fun getTuples(text: String): List<Pair<Float, String>> {
-        var produse = mutableListOf<Pair<Float, String>>()
+    fun getTuples(text: String): List<Pair<Double, String>> {
+        var produse = mutableListOf<Pair<Double, String>>()
         var produs_crt = 0
         var pret_crt = 0
         var nume_crt = 0
         var started = false
         val lines = text.split("\n")
         for (line in lines) {
-            if ("lei" in line.toLowerCase(Locale.getDefault())) {
-                started = true
-                continue
-            }
-            if (!started) {
-                continue
-            }
+           // if ("lei" in line.toLowerCase(Locale.getDefault())) {
+          //      started = true
+           //     continue
+         //   }
+         //   if (!started) {
+         //       continue
+        //    }
             if ("total" == line.toLowerCase(Locale.getDefault())) {
                 break
             }
@@ -106,20 +132,26 @@ class MainTextRecognizer : AppCompatActivity() {
             if (" buc" in line.toLowerCase(Locale.getDefault())) {
                 val words = line.split(' ')
                 if (words.size >= 4) {
-                    val nr = words[3].toFloat()
-                    if (pret_crt == produs_crt) {
-                        produse.add(Pair(nr, ""))
-                        produs_crt += 1
-                        pret_crt += 1
-                    } else {
-                        produse[pret_crt] = Pair(nr, produse[pret_crt].second)
-                        pret_crt += 1
-                    }
+                    if (words[3].toDoubleOrNull() != null) {
+                        val nr = words[3].toDouble()
+                        if (pret_crt == produs_crt) {
+                            produse.add(Pair(nr, "") as Pair<Double, String>)
+                            produs_crt += 1
+                            pret_crt += 1
+                        } else {
+                            produse[pret_crt] = Pair(nr, produse[pret_crt].second)
+                            pret_crt += 1
+                        }
+
+                        }
+
                 }
-            } else if (!line[0].isDigit() && "discount" !in line.toLowerCase(Locale.getDefault())
+            }
+
+            else if (!line[0].isDigit() && "discount" !in line.toLowerCase(Locale.getDefault())
                 && "total" !in line.toLowerCase(Locale.getDefault())){
                 if (nume_crt == produs_crt) {
-                    produse.add(Pair(0.toFloat(), line))
+                    produse.add(Pair(0.toDouble(), line))
                     produs_crt += 1
                     nume_crt += 1
                 } else {
