684af85df78c9f8786da591a3672ffdb45104fe3
Racovita Andra <andra.rac@gmail.com>
2020-06-11 22:52:18 +0300

    Delete Floating Action Button

--- app/src/androidTest/java/com/kip/receiptscanner/CheckListAndroidTest.kt
+++ app/src/androidTest/java/com/kip/receiptscanner/CheckListAndroidTest.kt
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class CheckListAndroidTest {
     @Rule
     @JvmField
     val rule: ActivityTestRule<ChecklistActivity> = ActivityTestRule(ChecklistActivity::class.java)
